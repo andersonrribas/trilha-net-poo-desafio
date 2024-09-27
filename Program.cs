ecc89a0fd65fbc505c0b400b21c7a165e033ee82
andersonrribas <ander.ribas@gmail.com>
2024-09-27 10:58:29 -0300

    Descrição: Resolução desafio Dio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("99999-1234", "Nokia 3310", "123456789012345", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Iphone iphone = new Iphone("88888-5678", "iPhone 14", "987654321098765", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
